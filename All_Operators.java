37a14a1d589a058b3bd8e74671227bf59e9313f2
KhushiKalwani24 <khushikalwani2412@gmail.com>
2026-02-25 23:46:44 +0530

    Add files via upload

---
+++ All_Operators.java
@@ -0,0 +1,13 @@
+package Bitwise_Operators;
+
+public class All_Operators {
+    
+    public static void main(String[] args) {
+        System.out.println((5 & 6));
+        System.out.println((5 | 6));
+        System.out.println((5 ^ 6));
+        System.out.println((~5));
+        System.out.println((5 << 2));
+        System.out.println((6 >> 1));
+    }
+}
